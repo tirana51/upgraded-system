e1cb4232ca94e2d836f6330210d031242bb7ad38
tirana51 <65135183+tirana51@users.noreply.github.com>
2020-05-11 11:41:22 +0530

    Create NEGBFRAG.py

---
+++ NEGBFRAG.py
@@ -0,0 +1,19 @@
+from fractions import Fraction as F
+def farey_seq(x, limit):
+    l, r = F(0), F(1)
+    while l.denominator + r.denominator <= limit:
+        m = F(l.numerator + r.numerator, l.denominator + r.denominator)
+        if m < x:
+            l = m
+        else:
+            r = m
+            if r == x:
+                break
+    if l.denominator + r.denominator <= limit:
+        k = 1 + (limit - (l.denominator + r.denominator)) // r.denominator
+        l = F(l.numerator + k * r.numerator, l.denominator + k * r.denominator)
+    return l
+for _ in range(int(input())):
+    a, b, n = map(int, input().split())
+    r = farey_seq(F(a, b), n)
+    print(r.numerator, r.denominator)
